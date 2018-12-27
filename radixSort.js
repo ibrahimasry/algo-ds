e3fbf95ac608e1a28b64509e07e7db563e48763f
ibrahim masry <ibrahim.elmansoury@gmail.com>
2018-12-27 16:46:23 +0200

    radix sort

--- radixSort.js
+++ radixSort.js
@@ -2,7 +2,7 @@
 
 function radixSort(arr){
     let buckets = Array.from({length:10}, ()=>[])
-    let maxLength = getMaxLength(arr)
+    const maxLength = getMaxLength(arr)
     for(let i = 0 ; i <= maxLength; i++){
       for(let j of arr){
         const digitBucket = getDigit(j, i)
@@ -22,8 +22,7 @@ function radixSort(arr){
     let max = -Infinity
   
     for(let num of arr) max = Math.max(num , max)
-    let length = Math.floor(Math.log10(max))
-    return length
+    return Math.floor(Math.log10(max))
   }
   
   //console.log(radixSort([1330,329,30,3,3,2,1,10]))
