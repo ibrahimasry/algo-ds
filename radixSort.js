a7ed26247f6a92f0ac70636676de343d5e9e35bf
ibrahim masry <ibrahim.elmansoury@gmail.com>
2018-12-27 16:44:55 +0200

    radix sort

---
+++ radixSort.js
@@ -0,0 +1,30 @@
+//radix sort with positive numbers
+
+function radixSort(arr){
+    let buckets = Array.from({length:10}, ()=>[])
+    let maxLength = getMaxLength(arr)
+    for(let i = 0 ; i <= maxLength; i++){
+      for(let j of arr){
+        const digitBucket = getDigit(j, i)
+        buckets[digitBucket].push(j)
+      }
+      arr = buckets.flat()
+      buckets = Array.from({length:10}, ()=>[])
+    }
+  
+    return arr
+  }
+  function getDigit(num, pos=0){
+    return Math.abs(Math.floor((num / 10 ** pos) % 10))
+  }
+  
+  function getMaxLength(arr){
+    let max = -Infinity
+  
+    for(let num of arr) max = Math.max(num , max)
+    let length = Math.floor(Math.log10(max))
+    return length
+  }
+  
+  //console.log(radixSort([1330,329,30,3,3,2,1,10]))
+  
